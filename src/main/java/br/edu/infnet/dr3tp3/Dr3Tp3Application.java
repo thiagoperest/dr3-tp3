a0fc57425f65a81ca5acba347f788eee266cd7ca
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-09-02 08:56:25 -0300

    chore: add base do projeto

---
+++ src/main/java/br/edu/infnet/dr3tp3/Dr3Tp3Application.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dr3tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr3Tp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dr3Tp3Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/infnet/dr3tp3/Dr3Tp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dr3tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dr3Tp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
